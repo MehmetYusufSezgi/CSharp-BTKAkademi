54c6c48d69cd194168673f6606e3d9e367e9a124
MehmetYusufSezgi <94776930+MehmetYusufSezgi@users.noreply.github.com>
2023-06-20 08:50:37 +0300

    Add files via upload

---
+++ C#-Github/Methods/Methods/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Added();
+            Added();
+            Added();
+            var result = Add1(20,30);
+            var result2 = Add2(20);
+            Console.WriteLine(result);
+            Console.WriteLine(result2);*/
+
+            /* number1 = 20;
+            int number2 = 60;
+
+            var result3 = Add3(ref number1,number2);
+            Console.WriteLine(result3);
+            Console.WriteLine(number1);*/
+
+            /*Console.WriteLine(multiply(10, 25));
+            Console.WriteLine(multiply(10, 25, 2));*/
+
+            Console.WriteLine(Add4(13, 84, 91));
+            Console.ReadLine();
+        }
+
+        static void Added()
+        {
+            Console.WriteLine("Added.");
+        }
+
+        static int Add1(int num1, int num2)
+        {
+            var result = num1 + num2;
+            return result;
+        }
+
+        static int Add2(int num1, int num2 = 20)
+        {
+            var result = num1 + num2;
+            return result;
+        }
+
+        static int Add3(ref int num1, int num2 = 20)
+        {
+            num1 = 35;
+            var result = num1 + num2;
+            return result;
+        }
+
+        static int multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        static int multiply(int num1, int num2, int num3)
+        {
+            return num1 * num2 * num3;
+        }
+
+        static int Add4(params int[] numbers)
+        {
+            var result = numbers.Sum();
+            return result;
+        }
+    }
+}
---
+++ C#-Github/TypesAndVariables/TypesAndVariables/Program.cs
@@ -0,0 +1,43 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypesAndVariables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("Hello world");
+            Console.ReadLine();*/
+            int number1 = 1000000000;
+            long number2 = 100000000000000000;
+            short number3 = 10000;
+            byte number4 = 255;
+            bool condition = true;
+            char character = 'A';
+            double number5 = 10.5;
+            decimal number6 = 10;
+            var number7 = 100;
+            number7 = 'X';
+
+            Console.WriteLine("Number1 is {0}", number1);
+            Console.WriteLine("Number2 is {0}", number2);
+            Console.WriteLine("Number3 is {0}", number3);
+            Console.WriteLine("Number4 is {0}", number4);
+            Console.WriteLine("Character is {0}", (int)character);
+            Console.WriteLine("Number5 is {0}", number5);
+            Console.WriteLine("Number7 is {0}", number7);
+            Console.WriteLine((int)Days.Sunday);
+
+
+            Console.ReadLine();
+        }
+    }
+    enum Days
+    {
+        Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+    }
+}
