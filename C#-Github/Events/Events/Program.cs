25e46ac5049715379aa884e8f1cc74a2769ad785
Mehmet Yusuf SEZGİ <94776930+MehmetYusufSezgi@users.noreply.github.com>
2023-07-04 10:48:46 +0300

    Add files via upload

---
+++ C#-Github/Events/Events/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product harddisk = new Product(50);
+            Product cellphone = new Product(50);
+
+            harddisk.ProductName = "Hard Disk";
+            cellphone.ProductName = "Cellphone";
+            cellphone.StockControlEvent += Cellphone_StockControlEvent;
+
+            for (int i = 0; i < 100; i++)
+            {
+                harddisk.Sell(10);
+                cellphone.Sell(10);
+                Console.ReadLine();
+            }
+            Console.ReadLine();
+        }
+
+        private static void Cellphone_StockControlEvent()
+        {
+            Console.WriteLine("!!! CELLPHONE STOCKS GETTING LOW !!!");
+        }
+    }
+}
